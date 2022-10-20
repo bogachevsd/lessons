231ea8f44a440818f2479a4dade796efa5ca0605
bogachevsd <stanislav.bogachev@gmail.com>
2022-10-20 20:42:39 +0300

    lesson1 all tasks

--- lesson1/lesson1/src/Main.java
+++ lesson1/lesson1/src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
-public class lesson_1 {
-    public static void Main(String[] args){
+public class Main {
+    public static void main(String[] args){
         //System.out.println("Hello world!");
         System.out.println("Задание 1.");
         int month = 3;
